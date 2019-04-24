118b5796b9ba81e04f106477218be65c85fad8fd
Morten Terhart <morten.terhart@gmail.com>
2019-04-24 19:44:40 +0200

    Added API method for obtaining a list of all documents

--- src/main/java/org/dhbw/mosbach/ai/cmd/services/DocumentService.java
+++ src/main/java/org/dhbw/mosbach/ai/cmd/services/DocumentService.java
@@ -7,19 +7,21 @@ import org.dhbw.mosbach.ai.cmd.model.Repo;
 import org.dhbw.mosbach.ai.cmd.model.User;
 import org.dhbw.mosbach.ai.cmd.response.Success;
 import org.dhbw.mosbach.ai.cmd.response.Unauthorized;
+import org.dhbw.mosbach.ai.cmd.services.payload.DocumentModel;
 import org.dhbw.mosbach.ai.cmd.util.CmdConfig;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.LocalDateTime;
+import java.util.List;
 
 @ApplicationScoped
 @Path(ServiceEndpoints.PATH_DOCUMENT)
@@ -38,7 +40,7 @@ public class DocumentService {
     @Path("/add")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addDocument(String documentName) {
+    public Response addDocument(DocumentModel documentModel) {
         if (request.getSession().getAttribute(CmdConfig.SESSION_IS_LOGGED_IN) == null) {
             return new Unauthorized().create("You have to login to be able to create a new document");
         }
@@ -47,14 +49,34 @@ public class DocumentService {
 
         Repo repository = repoDao.getRepo(user);
 
+        String documentName = documentModel.getName();
+
         Doc document = new Doc();
         document.setCuser(user);
+        document.setUuser(user);
         document.setRepo(repository);
-        document.setCtime(LocalDateTime.now());
         document.setName(documentName);
 
         docDao.createDoc(document);
 
         return new Success().create("Document was created successfully");
+    }
+
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllDocuments() {
+        if (request.getSession().getAttribute(CmdConfig.SESSION_IS_LOGGED_IN) == null) {
+            return new Unauthorized().create("You have to login to be able to fetch all documents");
+        }
+
+        User sessionUser = (User) request.getSession().getAttribute(CmdConfig.SESSION_USER);
+
+        List<Doc> ownerDocs = docDao.getDocsOwnedBy(sessionUser);
+        List<Doc> collaboratorDocs = docDao.getDocsCollaboratedBy(sessionUser);
+
+        ownerDocs.addAll(collaboratorDocs);
+
+        return Response.ok().entity(ownerDocs).build();
     }
 }

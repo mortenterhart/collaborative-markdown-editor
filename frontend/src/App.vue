86af5c9831fe236c3dda92af5151c7e1f25e17f0
Michael Skrzypietz <michael.skr97@gmail.com>
2019-05-02 17:58:25 +0200

    added vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,30 +28,15 @@
         </v-toolbar>
         <v-content>
             <Login/>
-            <template v-if="false">
-                <Welcome />
-            </template>
-            <template v-else>
-                <v-container fluid fill-height>
-                    <v-layout row>
-                        <v-flex xs6 pr-2>
-                            <MDE @contentWasChanged="content = $event"/>
-                        </v-flex>
-                        <v-flex xs6 pl-2>
-                            <Preview :content="content"/>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </template>
+            <!-- route outlet -->
+            <!-- component matched by the route will render here -->
+            <router-view></router-view>
         </v-content>
         <vue-snotify></vue-snotify>
     </v-app>
 </template>
 
 <script>
-    import Welcome from './components/Welcome'
-    import MDE from './components/Editor'
-    import Preview from './components/Preview'
     import Login from './components/Login'
     import Drawer from "./components/Drawer";
     import Cookies from 'js-cookie';
@@ -59,15 +44,11 @@
     export default {
         name: 'App',
         components: {
-            Welcome,
             Drawer,
-            MDE,
-            Preview,
             Login
         },
         data() {
             return {
-                content: '',
                 drawer: false,
                 title: 'Collaborative Markdown Editor'
             }

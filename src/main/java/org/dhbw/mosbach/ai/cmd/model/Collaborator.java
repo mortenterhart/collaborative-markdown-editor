05717329108a4a6546b9af104982f0c2a8e00d88
MSINF16B <mic.spahr.16@lehre.mosbach.dhbw.de>
2019-05-02 10:01:59 +0200

    Added equals and hashCode methods to models

--- src/main/java/org/dhbw/mosbach/ai/cmd/model/Collaborator.java
+++ src/main/java/org/dhbw/mosbach/ai/cmd/model/Collaborator.java
@@ -80,6 +80,49 @@ public class Collaborator {
     }
 
     @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ctime == null) ? 0 : ctime.hashCode());
+		result = prime * result + ((doc == null) ? 0 : doc.hashCode());
+		result = prime * result + ((hasAccess == null) ? 0 : hasAccess.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Collaborator other = (Collaborator) obj;
+		if (ctime == null) {
+			if (other.ctime != null)
+				return false;
+		} else if (!ctime.equals(other.ctime))
+			return false;
+		if (doc == null) {
+			if (other.doc != null)
+				return false;
+		} else if (!doc.equals(other.doc))
+			return false;
+		if (hasAccess != other.hasAccess)
+			return false;
+		if (id != other.id)
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+	@Override
     public String toString() {
         return new StringBuilder()
             .append("User: \n")

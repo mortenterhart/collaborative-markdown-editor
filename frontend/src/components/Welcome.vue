41fa767945ce3b46c69feb4ec1cb4dfbe672cbc8
Michael Skrzypietz <michael.skr97@gmail.com>
2019-05-08 20:23:02 +0200

    fixed weird boxes in party img

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -82,18 +82,6 @@
                 <v-layout column align-center justify-center>
                     <div class="headline white--text mb-3 text-xs-center">Stay tuned for exciting new features you didn't knew you wanted</div>
                 </v-layout>
-                <v-layout justify-space-around justify-center>
-
-                    <v-icon x-large dark>fab fa-facebook-f</v-icon>
-
-                    <v-icon x-large dark>fab fa-twitter</v-icon>
-
-                    <v-icon x-large dark>fab fa-reddit-alien</v-icon>
-
-                    <v-icon x-large dark>fab fa-instagram</v-icon>
-
-                    <v-icon x-large dark>fab fa-discord</v-icon>
-                </v-layout>
             </v-parallax>
         </section>
 

c3425e28337e6597279e02612075a088eea9e823
Michael Skrzypietz <michael.skr97@gmail.com>
2019-05-11 09:53:59 +0200

    added chat window

--- frontend/src/components/Document.vue
+++ frontend/src/components/Document.vue
@@ -1,5 +1,20 @@
 <template>
     <v-container fluid fill-height>
+        <beautiful-chat
+                :participants="this.$store.state.app.otherCollaborators"
+                :onMessageWasSent="onMessageWasSent"
+                :messageList="messageList"
+                :newMessagesCount="newMessagesCount"
+                :isOpen="isChatOpen"
+                :close="closeChat"
+                :open="openChat"
+                :showEmoji="true"
+                :showFile="false"
+                :showTypingIndicator="showTypingIndicator"
+                :colors="colors"
+                :alwaysScrollToBottom="alwaysScrollToBottom"
+                :messageStyling="messageStyling"
+                @onType="handleOnType" />
         <v-layout row>
             <v-flex xs6 pr-2>
                 <MDE :key="this.$store.state.app.editorKey" @contentWasChanged="content = $event"/>
@@ -17,15 +32,75 @@
 
     export default {
         name: "Document",
-        data: () => ({
-            content: ''
-         }),
+        data() {
+            return {
+                content: '',
+                messageList: [], // the list of the messages to show, can be paginated and adjusted dynamically
+                newMessagesCount: 0,
+                isChatOpen: false, // to determine whether the chat window should be open or closed
+                showTypingIndicator: '', // when set to a value matching the participant.id it shows the typing indicator for the specific user
+                colors: {
+                    header: {
+                        bg: '#4e8cff',
+                        text: '#ffffff'
+                    },
+                    launcher: {
+                        bg: '#4e8cff'
+                    },
+                    messageList: {
+                        bg: '#ffffff'
+                    },
+                    sentMessage: {
+                        bg: '#4e8cff',
+                        text: '#ffffff'
+                    },
+                    receivedMessage: {
+                        bg: '#eaeaea',
+                        text: '#222222'
+                    },
+                    userInput: {
+                        bg: '#f4f7f9',
+                        text: '#565867'
+                    }
+                }, // specifies the color scheme for the component
+                alwaysScrollToBottom: false, // when set to true always scrolls the chat to the bottom when new events are in (new message, user starts typing...)
+                messageStyling: true // enables *bold* /emph/ _underline_ and such (more info at github.com/mattezza/msgdown)
+            }
+        },
         components: {
             MDE,
             Preview
         },
         created() {
             // TODO: check if user is logged in and if user has access to the doc
+        },
+        methods: {
+            sendMessage(text) {
+                if (text.length > 0) {
+                    this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1
+                    this.onMessageWasSent({author: 'support', type: 'text', data: {text}})
+                }
+            },
+            onMessageWasSent(message) {
+                // called when the user sends a message
+                this.messageList = [...this.messageList, message]
+            },
+            openChat() {
+                // called when the user clicks on the fab button to open the chat
+                this.isChatOpen = true
+                this.newMessagesCount = 0
+            },
+            closeChat() {
+                // called when the user clicks on the botton to close the chat
+                this.isChatOpen = false
+            },
+            handleScrollToTop() {
+                // called when the user scrolls message list to top
+                // leverage pagination for loading another page of messages
+            },
+            handleOnType() {
+                // Emit typing event
+            }
         }
     }
 </script>

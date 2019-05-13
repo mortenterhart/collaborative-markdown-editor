d243587167e173e4c702853a75af85c3f9d52d9c
Michael Skrzypietz <michael.skr97@gmail.com>
2019-05-13 13:55:04 +0200

    Updated forbidden page

--- frontend/src/components/ForbiddenPage.vue
+++ frontend/src/components/ForbiddenPage.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        403 Unauthorized
+        403 Forbidden. You have no access to this document!
     </div>
 </template>
 
 <script>
     export default {
-        name: "UnauthorizedPage"
+        name: "ForbiddenPage"
     }
 </script>
 
